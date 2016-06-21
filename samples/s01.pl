93d3e2b20d43cb79efb8ae12aea4b1b2a6eb1bb6
Reini Urban <rurban@cpan.org>
2016-06-21 11:19:35 +0200

    add comments about how to avoid feature bloat

--- samples/s01.pl
+++ samples/s01.pl
@@ -3,13 +3,16 @@
 use strict;
 use warnings;
 
+# This is a bad example of how to add feature bloat. say needs only 5.10 
 use v5.022;
 
 say q{# @INC:};
+# better written as print "$_\n" for @INC;
 map { say $_ } @INC;
 
 say '';
 say q{# %INC:};
+# better written as print "$_ => $INC{$_}\n" for sort keys %INC;
 map { say $_, ' => ', $INC{$_} } sort keys %INC;
 
 1;
